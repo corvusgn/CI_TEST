c4131232c4c460eb4729e681831349c8bbfd38f0
Gustavo <corvusgn@gmail.com>
2020-09-07 17:03:45 -0500

    Cambio 14

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get('/service',(req, res) => {
-    res.send({env: envName, response: 'cambio13 --- ' + new Date().getTime()});
+    res.send({env: envName, response: 'cambio14 --- ' + new Date().getTime()});
 })
 
 
